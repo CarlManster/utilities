47e48cad86855ba707953b54a2e290f3a360fdc5
최종민[Note App] <jm.choi@navercorp.com>
2026-04-13 14:55:10 +0900

    Add cookie for last view sub page

--- script.js
+++ script.js
@@ -6,5 +6,13 @@ function getLang() {
 function navigate(el) {
   document.querySelectorAll('.nav-link').forEach(function(a) { a.classList.remove('active'); });
   el.classList.add('active');
-  document.getElementById('content').src = el.dataset.src + '?lang=' + getLang();
+  var src = el.dataset.src;
+  var d = new Date(); d.setFullYear(d.getFullYear() + 1);
+  document.cookie = 'utilities_page=' + encodeURIComponent(src) + ';expires=' + d.toUTCString() + ';path=/';
+  document.getElementById('content').src = src + '?lang=' + getLang();
+}
+
+function getSavedPage() {
+  var m = document.cookie.match(/(^|;\s*)utilities_page=([^;]*)/);
+  return m ? decodeURIComponent(m[2]) : '';
 }
